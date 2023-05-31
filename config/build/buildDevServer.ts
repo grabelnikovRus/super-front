b2f25a955188f2178c4bca3337a0167073a814d3
ruslan <grabelnikov.rus@mail.ru>
2023-05-31 23:40:25 +0700

    master: add Hot Module Replacement

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,6 +3,7 @@ import { BuildOptions } from "./types/config"
 
 export function buildDevSrver(port: BuildOptions["port"]): DevServerConfiguration {
     return {
+        hot: true, // использует обновление страницы без перезашрузки
         port,
         open: true,
         historyApiFallback: true, // позволяет запускать не с главной страницы приложение
