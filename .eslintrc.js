f4055ba8ef45a3f5636875803d69c0e5cbedd654
ruslan <grabelnikov.rus@mail.ru>
2023-06-01 14:04:09 +0700

    master: add eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard-with-typescript"
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.eslint.json"
+  },
+  plugins: [
+    "react"
+  ],
+  rules: {
+    quotes: ["error", "double"],
+    "max-len": ["warn", { code: 90, tabWidth: 2 }],
+    "@typescript-eslint/quotes": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/semi": "off",
+    "@typescript-eslint/space-before-function-paren": "off",
+    "@typescript-eslint/comma-dangle": "off"
+  }
+}
