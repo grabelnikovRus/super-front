8ea1e8ac28e57b8ad28591c3b4125e4b64a1e03e
ruslan <grabelnikov.rus@mail.ru>
2023-06-19 14:29:11 +0700

    master: BundleAnalyzer disable in prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,15 +8,19 @@ export function buildPlugins(
   paths: BuildPaths["html"],
   isDev: BuildOptions["isDev"]
 ): WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({ template: paths }),
     new MiniCssExtractPlugin({
       filename: "css/[name].[contenthash].css",
       chunkFilename: "[id].css",
-    }), // извлекает CSS в отдельные файлы.Он создает CSS-файл для каждого JS-файла,который содержит CSS.Он поддерживает загрузку CSS и SourceMaps по требованию.
+    }), // извлекает CSS в отдельные файлы.Он создает CSS-файл для каждого JS-файла,который содержит CSS.
+    // Он поддерживает загрузку CSS и SourceMaps по требованию.
     // MiniCssExtractPlugin также содержит статический метод loader
     new ProgressPlugin(),
     new DefinePlugin({ _IS_DEV_: isDev }),
-    new BundleAnalyzerPlugin()
-  ];
+  ]
+
+  if (isDev) plugins.push(new BundleAnalyzerPlugin())
+
+  return plugins
 }
