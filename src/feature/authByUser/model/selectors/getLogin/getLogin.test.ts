e99d14f8875c1c8ee12fc8d3e433a4cd4acccd27
ruslan <grabelnikov.rus@mail.ru>
2023-06-29 12:14:22 +0700

    master: add getLogin.tets

---
+++ src/feature/authByUser/model/selectors/getLogin/getLogin.test.ts
@@ -0,0 +1,25 @@
+import { type DeepPartial } from "@reduxjs/toolkit"
+import { getLogin } from "./getLogin"
+import { type StateType } from "@app/providers/storeProvider"
+
+describe("getLogin", () => {
+  test("return state login", () => {
+    const state: DeepPartial<StateType> = {
+      login: {
+        username: "123",
+        password: "123",
+        isLoading: true
+      }
+    }
+    expect(getLogin(state as StateType)).toEqual({
+      username: "123",
+      password: "123",
+      isLoading: true
+    })
+  })
+
+  test("empty state", () => {
+    const state: DeepPartial<StateType> = {}
+    expect(getLogin(state as StateType)).toBe(undefined)
+  })
+})
