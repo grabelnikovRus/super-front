868a0e40f09768b03fc922a5d0fbaf5cf8ab744c
ruslan <grabelnikov.rus@mail.ru>
2023-07-06 18:47:33 +0700

    master: addition Article

--- src/shared/ui/article/Article.tsx
+++ src/shared/ui/article/Article.tsx
@@ -1,21 +1,29 @@
 import { type FC } from "react"
+import { classNames } from "@shared/helpers/lib"
 
 import s from "./Article.module.scss"
-import { classNames } from "@shared/helpers/lib"
 
 type theme = "default" | "error"
 
+type align = "center" | "left" | "rigth"
+
 interface ArticleProps {
-  title?: string
-  text?: string
+  title?: string | null
+  text?: string | null
   theme?: theme
+  align?: align
 }
 
-export const Article: FC<ArticleProps> = ({ title, text, theme = "default" }) => {
+export const Article: FC<ArticleProps> = ({
+  title,
+  text,
+  theme = "default",
+  align = "left"
+}) => {
   if (!text && !title) return null;
 
   return (
-    <article className={classNames(s.article, {
+    <article className={classNames(s.article, s[align], {
       [s.article__error]: theme === "error",
     })}>
       {title && <h6 className={s.article_title}>{title}</h6>}
