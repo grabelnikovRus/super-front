a9fa7bb19f2eb6276453f9d9c4954e05a638dd58
ruslan <grabelnikov.rus@mail.ru>
2023-07-10 23:48:32 +0700

    master: add translation on Page

--- src/shared/config/i18n/configForStorybook.ts
+++ src/shared/config/i18n/configForStorybook.ts
@@ -3,7 +3,7 @@ import i18n, { type ResourceLanguage } from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-const ns = ["translation"];
+const ns = ["translation", "profile"];
 const supportedLngs = ["en", "ru"];
 
 const resources = ns.reduce((acc: Record<string, ResourceLanguage>, n) => {
