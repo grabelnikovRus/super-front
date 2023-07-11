d97f30ad2d0f2d5f609d8e3f4927968a4697d862
ruslan <grabelnikov.rus@mail.ru>
2023-07-11 13:51:15 +0700

    master: add tests slice profile

---
+++ src/feature/editableProfileCard/model/slice/profileSlice.test.ts
@@ -0,0 +1,50 @@
+import { Country, Currency } from "@shared/constants/common"
+import { type ProfileScheme } from "../types/types"
+import { profileReducer, profileActions } from "./profileSlice"
+import { udpateProfile } from "../services/udpateProfile"
+
+describe("profileSlice", () => {
+  const profile = {
+    name: "Ruslan",
+    age: 33,
+    currency: Currency.RUB,
+    country: Country.BLR,
+    city: "Tomsk"
+  }
+
+  const state: ProfileScheme = {
+    form: profile,
+    data: profile,
+    readonly: true,
+    isLoading: false,
+    error: ""
+  }
+
+  test("set readonly", () => {
+    expect(profileReducer(
+      state,
+      profileActions.editProfile(false)
+    )).toEqual({ ...state, readonly: false })
+  })
+
+  test("set field profile data", () => {
+    expect(profileReducer(
+      state,
+      profileActions.setStringProfile({ age: 34 })
+    )).toEqual({ ...state, form: { ...state.form, age: 34 } })
+  })
+
+  test("test extra reducer pending", () => {
+    expect(profileReducer(
+      state,
+      udpateProfile.pending
+    )).toEqual({ ...state, isLoading: true })
+  })
+
+  test("test extra reducer fulfilled", () => {
+    expect(profileReducer(
+      { ...state, data: {}, form: {} },
+      udpateProfile.fulfilled(profile, "profile/udpateProfile", undefined)
+    )).toEqual(state)
+  })
+})
